d0af6500c8848c08924d57dfb9e91a1cc06c050d
donlHenard <pastor.git101@gmail.com>
2023-03-03 18:52:56 +0800

    Finsihed 2nd exercise
    easy enough for me. I find it fascinating on how this test is being done
    is this what they use in coding or this is just specific for javascript
    use only

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
 
+    let repeatedWord = "";
+
+    if (times < 0 ){
+        return "ERROR";
+    } else {
+        for (let i = 1; i <= times; i++) { 
+            repeatedWord += word;
+        }
+    
+        return repeatedWord;
+    }
+
+    
 };
 
 // Do not edit below this line
