2c400323e044e65ec5172de60ef239bbdafc3ad7
donlHenard <pastor.git101@gmail.com>
2023-03-08 17:28:07 +0800

    Finished third activity
    Its more on the array methods that did the job. first string to array by
    using split and using no space as parameter which basically takes every
    letter and space as an individual elements. Then use the reverse method
    for the array then use join method to with parameter no space to convert
    it back to string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let stringArray = string.split("");
+    
+    stringArray.reverse();
+
+    let reversedString = stringArray.join("");
+
+    return reversedString;
 };
 
 // Do not edit below this line
