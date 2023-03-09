6240a6f77917efbe2cce332bb0f1e01341c78d10
donlHenard <pastor.git101@gmail.com>
2023-03-09 17:15:49 +0800

    Finish 5th activity
    I think I did badly meaning I think their are better way on how to do
    this but nevertheless I did it

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(range1, range2) {
+
+    if (typeof range1 == "number" &&  typeof range2 == "number"){
+
+        if ( range1 < 0 || range2 < 0 ) {
+            return "ERROR";
+        } else {
+            
+            if (range1 > range2) {
+        
+                let tempRange1 = range1;
+                range1 = range2;
+                range2 = tempRange1;
+            } 
+        }
+
+    } else {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+
+    for (let i = range1; i <= range2; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
