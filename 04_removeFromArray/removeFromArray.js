0c04a09efbd65a31dc6531228b45cfaf476df6f8
donlHenard <pastor.git101@gmail.com>
2023-03-09 12:25:19 +0800

    Finished 4th activity
    first take the arguments convert to array then use shift to take the the
    array in the arguments which is first element then use nested for loop
    to loop both array then use if with condition that if the element is
    equal upto typeof then it will use splice method to take out the element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
 const removeFromArray = function() {
 
+    let parameter = Array.from(arguments);
+
+    let array = parameter.shift();
+
+    for (let i = 0; i < parameter.length; i++) {
+        
+        for (let j = 0; j <= array.length; j++){
+            
+            if(parameter[i] === array[j]) {
+
+                array.splice(j,1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
