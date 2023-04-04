fcd02dc032db086bc4fe95ec769f3a45d359dd67
donlHenard <pastor.git101@gmail.com>
2023-04-04 15:50:05 +0800

    finished this activity before solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,33 @@
-const palindromes = function () {
+const palindromes = function (word) {
+   let letters = word.split("");
+   let noSpace = "";
+   let reversedArray = [];
 
+        for(let letter of letters){
+            switch(letter){
+                case " ":
+                    break;
+                case "!":
+                    break;
+                case ",":
+                    break;
+                case ".":
+                    break;      
+                default:
+                    noSpace += letter;
+                    reversedArray.push(letter);
+            }
+        }
+    let reversed = "";
+        for( let letter of reversedArray.reverse()){
+            reversed += letter;
+        }
+    
+   if(noSpace.toLowerCase() === reversed.toLowerCase()){
+       return true;
+   } else{
+      return false;
+    }
 };
 
 // Do not edit below this line
