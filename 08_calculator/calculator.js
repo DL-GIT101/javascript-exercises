2c3edad236f3e6c2524bc9708682afb425792400
donlHenard <pastor.git101@gmail.com>
2023-04-04 11:51:57 +0800

    finished the activity before solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(adds) {
+  let total = 0;
+	for (let add of adds){
+    total += add;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(multipliers) {
+  let product = 1;
+	for (let multiplier of multipliers){
+    product *= multiplier;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num,exponent) {
+  let product =1;
+	for(let i = 0; i < exponent; i++){
+    product *= num;
+  }return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(natural) {
+	let result = 1;
+  if(natural == 0){
+    return result;
+  } else {
+    for(let i = 1; i <= natural; i++){
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
