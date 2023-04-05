403837ccd2a4965178e995b4e26ad907934a7f7d
donlHenard <pastor.git101@gmail.com>
2023-04-05 11:46:55 +0800

    finished activity before solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequence) {
+    if (sequence < 0){
+        return "OOPS";
+    }
+    let first = 0;
+    let second = 1;
+    let sum;
+    for(let i = 1; i < sequence; i++){
+        sum = first + second;
+        first = second;
+        second = sum;
+    }
+        return second;
 };
 
 // Do not edit below this line
