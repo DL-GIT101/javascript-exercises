c3b533b270b0614af9d5a936475827cb5a72eaf2
donlHenard <pastor.git101@gmail.com>
2023-04-07 11:41:36 +0800

    finished activity before solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    for (let person of people){
+        
+        if(person.yearOfDeath  === undefined){
+            const d = new Date();
+            person.yearOfDeath = d.getFullYear();
+        }
+    }
+    people.sort((a,b) =>{
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth) 
+    });
+    return people[0];
 
 };
 
